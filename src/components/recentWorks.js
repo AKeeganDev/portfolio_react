ab3f85ad4cba4a5003fa7e3ed8077b681b70add6
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-09-15 17:44:27 -0400

    add recent work header

---
+++ src/components/recentWorks.js
@@ -0,0 +1,18 @@
+import '../stylesheets/recentWorks.scss';
+
+const RecentWorks = () => {
+  console.log('saving space');
+
+  return (
+    <section className="recent-works" id="portfolio">
+      <div className="recent-work-heading-container">
+        <div className="heading">
+          <h1>My Recent Works</h1>
+        </div>
+        <div className="recent-work-divider" />
+      </div>
+    </section>
+  );
+};
+
+export default RecentWorks;
